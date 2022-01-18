f553cc7be618fd43f839b1308891a96d4ac6e9d7
Elale1984 <69740507+Elale1984@users.noreply.github.com>
2022-01-17 23:25:54 -0600

    init

---
+++ app/src/main/java/us/libertybaptistchurch/mylibertybaptist/MainActivity.kt
@@ -0,0 +1,38 @@
+package us.libertybaptistchurch.mylibertybaptist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import us.libertybaptistchurch.mylibertybaptist.ui.theme.MyLibertyBaptistTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyLibertyBaptistTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyLibertyBaptistTheme {
+        Greeting("Android")
+    }
+}
